178f254f094b97d57b37538da7845e91a45ce767
felesiaj <felesiaj26@gmail.com>
2025-11-05 16:20:15 +0700

    tutorial 7: done

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/widgets/left_drawer.dart';
-import 'package:football_news/newslist_form.dart';
+import 'package:football_news/screens/newslist_form.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
